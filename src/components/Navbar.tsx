659b7a3b0a641aff9e3e5e6ecfa41e1e26a9857b
kenyounot123 <kenlu519@gmail.com>
2024-08-24 18:26:38 -0400

    Fix merge errors in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-=======
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "./ui/button"
->>>>>>> transcript-page-layout
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center py-2 w-[95%] mx-auto">
       <div className="flex items-center gap-2">
         <div>
-<<<<<<< HEAD
           <Link href={"/"}>
             <Image
               src={"/favicons/favicon-32x32.png"}
@@ -25,13 +18,6 @@ export default function Navbar() {
           </Link>
         </div>
         <Link href={"/"}>
-=======
-          <Link href={'/'}>
-            <Image src={"/favicons/favicon-32x32.png"} alt="VoiceTaker" width={32} height={32}/>
-          </Link>
-        </div>
-        <Link href={'/'}>
->>>>>>> transcript-page-layout
           <h3>VoiceTaker</h3>
         </Link>
       </div>
