8f2610bbb17e23a6f3400d487545e21199dec927
bloopgoop <zhu.kevin12@gmail.com>
2024-08-24 14:02:56 -0400

    add crud with aws dynamodb and lambda

---
+++ src/app/actions.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { Comment } from "@/types";
+
+console.log(process.env.AWS_URL);
+
+export async function createComment({ comment }: { comment: Comment }) {
+  const response = await fetch(`${process.env.AWS_URL!}/items`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(comment),
+  });
+  console.log(JSON.stringify(comment));
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("Failed to create comment");
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+export async function getComments() {
+  const response = await fetch(`${process.env.AWS_URL!}/items`);
+  if (!response.ok) {
+    throw new Error("Failed to create comment");
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+export async function getComment({ id }: { id: string }) {
+  const response = await fetch(`${process.env.AWS_URL!}/items/${id}`);
+  if (!response.ok) {
+    throw new Error("Failed to create comment");
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;  
+}
+
+export async function deleteComment({ id }: { id: string }) {
+  const response = await fetch(`${process.env.AWS_URL!}/items/${id}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to create comment");
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+export interface Comment {
+    id: string;
+    user_id: string;
+    transcript_id: string;
+    transcript_text: string;
+    comment: string;
+}
