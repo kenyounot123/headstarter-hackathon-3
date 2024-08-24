26d09679fae1490ae42a4b426f66687f72c2c16e
bloopgoop <zhu.kevin12@gmail.com>
2024-08-24 16:11:23 -0400

    add open ai completions routes

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,3 +5,11 @@ export interface Comment {
     transcript_text: string;
     comment: string;
 }
+
+export interface Message {
+    time: string;
+    name: string;
+    dialogue: string;
+}
+
+export type Transcript = Message[];
