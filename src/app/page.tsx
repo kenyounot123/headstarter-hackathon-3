a3522c4cce2716e3c47568729693c150704cf90c
kenyounot123 <kenlu519@gmail.com>
2024-08-25 10:55:58 -0400

    Add demo video to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
+interface VideoProps {
+  className: string
+}
+function Video({className}:VideoProps) {
+  return (
+    <video className={className}
+    width="0" 
+    height="0"
+    playsInline 
+    autoPlay 
+    muted
+    loop 
+    preload="auto">
+      <source src="/demo.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  )
+}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 mt-5 pt-5">
@@ -10,14 +26,8 @@ export default function Home() {
       <div>
         <Link href={'/transcript'}><Button size={"lg"} className="text-xl">Get Started</Button></Link>
       </div>
-      <div className="relative min-w-[300px] max-w-[800px] min-h-96 w-[90%] bg-slate-900 rounded shadow shadow-lg">
-        {/* <Image
-          src=""
-          alt="Placeholder"
-          layout="fill"
-          objectFit="cover"
-          className="absolute inset-0"
-          /> */}
+      <div className="relative min-w-[300px] max-w-[800px] min-h-96 w-[90%] aspect-[16/9] shadow shadow-xl">
+        <Video className={'w-full h-full object-cover rounded'}/>
       </div>
       {/* <div className="min-h-24 w-full max-w-[800px] h-26 absolute -z-99 bottom-24 right-0">
         <Image src={"/sound1.png"} alt="VoiceTaker" width={0} height={0} className="w-full h-[200px] object-cover" sizes="100vw"/>
